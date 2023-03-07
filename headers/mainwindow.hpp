0050eca93d972b2ef24093824bfeb97518814430
superlulu90 <lucas.ferry.ecole@gmail.com>
2023-03-07 17:00:06 +0100

    give up multiple object and adding immobile player

--- headers/mainwindow.hpp
+++ headers/mainwindow.hpp
@@ -2,11 +2,11 @@
 #define MAINWINDOW_HPP
 
 #include <QWidget>
-
 class MainWindow : public QWidget
 {
     public:
     explicit MainWindow(QWidget *parent = nullptr);
+    void drawPlayer(QPainter &painter);
 
     protected:
     void paintEvent(QPaintEvent* event);
--- sources/mainwindow.cpp
+++ sources/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "../headers/mainwindow.hpp"
 #include <QPainter>
 #include <QPaintEvent>
+#include <cmath>
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
@@ -14,6 +15,8 @@
 
 #define CASE WHEIGHT/WORLD_SIZE
 
+#define PLAYER_SIZE 15
+
 int world_map[WORLD_SIZE][WORLD_SIZE] = {
     { 1, 1, 1, 1, 1, 1, 1, 1 },
     { 1, 0, 1, 0, 0, 0, 0, 1 },
@@ -25,9 +28,25 @@ int world_map[WORLD_SIZE][WORLD_SIZE] = {
     { 1, 1, 1, 1, 1, 1, 1, 1 },
 };
 
+/* Player Variables */
+float player_x = 450.0;
+float player_y = 650.0;
+float player_a = 0.0;
+
 MainWindow::MainWindow (QWidget *parent) : QWidget(parent){
     setFixedSize(WWIDTH+1, WHEIGHT+1);
     setWindowTitle("Raycasting with QT");
+}
+
+void MainWindow::drawPlayer(QPainter &painter){
+    painter.setPen(QPen(Qt::red, 1, Qt::SolidLine, Qt::RoundCap));
+    painter.setBrush(QBrush(Qt::red, Qt::SolidPattern));
+    painter.drawEllipse(player_x, player_y, PLAYER_SIZE, PLAYER_SIZE);
+    painter.drawLine(
+        player_x+PLAYER_SIZE/2,
+        player_y+PLAYER_SIZE/2, 
+        player_x+PLAYER_SIZE/2+cos(player_a)*30, 
+        player_y+PLAYER_SIZE/2+sin(player_a)*30);
 }
 
 void MainWindow::paintEvent(QPaintEvent* event){
@@ -56,10 +75,14 @@ void MainWindow::paintEvent(QPaintEvent* event){
         }
     }
     /* End of draw the 2d map */
+
+    /* Draw the Player */
+    drawPlayer(painter);
     
+    /* End of Draw the Player */
+
     /* Draw the 3d map */
     painter.setRenderHint(QPainter::Antialiasing);
-
     /* End of draw the 3d map */
 
     /* Separators */
